4c918142383ba563b3478473c85201b593f57c1c
Kozo Simutanyi <s3750253@student.rmit.edu.au>
2020-08-20 00:26:26 +1000

    Add project files.

---
+++ MiBank_A3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiBank_A3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
